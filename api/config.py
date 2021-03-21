1e2e4b44de184bf26163753fc41ccda88bbe4eba
David Sarpong <sarpong.david2@gmail.com>
2021-03-21 19:11:29 +0000

    minor fix

--- api/config.py
+++ api/config.py
@@ -8,6 +8,7 @@ dotenv.load_dotenv(verbose = True, dotenv_path = ENV_PATH)
 class Config:
    DEBUG = os.getenv('DEBUG', True)
    SECRET_KEY = os.getenv('SECRET_KEY', str(secrets.token_hex(16)))
+   DATABASE_URL = os.getenv('DATABASE_URL', "mysql://bff87691226f29:44c2d26c@us-cdbr-east-03.cleardb.com/heroku_4d5eaecda761fa0?reconnect=true")
    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', "mysql://root@localhost/universitydb")
    MAIL_SERVER = os.getenv('MAIL_SERVER', "smtp.googlemail.com")
    MAIL_PORT = os.getenv('MAIL_PORT', 587)
